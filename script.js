78f086ca6d1ba40b65deb05d326d255ae0660fce
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-07-14 20:05:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,20 +13,20 @@ let bowlerScorecard = [];
 let bowlerKeys = ["playerName", "overs", "maidenOvers", "runs", "wickets", "noBalls", "wideBalls", "economy"];
 
 async function main(){
-    await browser.get("https://www.cricbuzz.com/live-cricket-scores/" + matchId); // -> open site 
-    await browser.wait(wd.until.elementLocated(wd.By.css(".cb-nav-bar a"))); // -> wait till element got load (safe when internet slow)
-    let buttons = await browser.findElements(wd.By.css(".cb-nav-bar a"));  // -> find kro cb-nav-bar ke andar ke sare a tag and buttons m store karo
-    await buttons[1].click();  // -> scoreboard button
+    await browser.get("https://www.cricbuzz.com/live-cricket-scores/" + matchId); 
+    await browser.wait(wd.until.elementLocated(wd.By.css(".cb-nav-bar a"))); 
+    let buttons = await browser.findElements(wd.By.css(".cb-nav-bar a")); 
+    await buttons[1].click();  
     // console.log(buttons.length);
-    await browser.wait(wd.until.elementLocated(wd.By.css(" .cb-col.cb-col-100.cb-ltst-wgt-hdr"))); // -> wait till element got load (safe when internet slow)
+    await browser.wait(wd.until.elementLocated(wd.By.css(" .cb-col.cb-col-100.cb-ltst-wgt-hdr"))); 
     let tables = await browser.findElements(wd.By.css("#innings_" + innings + " .cb-col.cb-col-100.cb-ltst-wgt-hdr"));
     // console.log(tables.length);
-    let innings1BatsmenRows = await tables[0].findElements(wd.By.css(".cb-col.cb-col-100.cb-scrd-itms")); // -> scorecard ki sari rows aa jayegi of each player
+    let innings1BatsmenRows = await tables[0].findElements(wd.By.css(".cb-col.cb-col-100.cb-scrd-itms"));
     // console.log(innings1BatsmenRows.length);
     for(let i = 0; i < innings1BatsmenRows.length; i++) {
-        let columns = await innings1BatsmenRows[i].findElements(wd.By.css("div")); //-> sare columns le aye of each row
+        let columns = await innings1BatsmenRows[i].findElements(wd.By.css("div"));
         // console.log(columns.length);
-        if(columns.length == 7) { // -> jinki column ki length 7 hai vhi batsman h vhi chye bs (no need of extras etc) 
+        if(columns.length == 7) { 
             let data = {};
             for(let j = 0; j < columns.length; j++) {
                 // let temp = await columns[j].getAttribute("innerText");
